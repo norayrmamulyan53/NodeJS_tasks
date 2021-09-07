b5e8946c1774d8b926929f27b7c08629e0e23ce2
norayrsoftware <norayr.software@gmail.com>
2021-09-07 21:19:15 +0400

    Getting started in NodeJS

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs =require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+const server = http.createServer((req,res) => {
+    fs.readFile('./text',(err,data) => {
+        if(err) throw err
+        console.log(data.toString())
+    })
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
