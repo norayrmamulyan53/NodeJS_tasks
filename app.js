4fbfed61323b6fd6a4b3dac1a9bcc63aed684155
norayrsoftware <norayr.software@gmail.com>
2021-09-07 22:36:52 +0400

    Switching to I/O files via file's full name

--- app.js
+++ app.js
@@ -8,24 +8,14 @@ const port = 3000;
 const server = http.createServer((req,res) => {
     const path = req.url;
     const fsCallback = (err,data) => {
-        if(err) throw err;
+        if(err) {
+            res.end("We couldn't find your page :(")
+        };
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
         res.end(data)
     }
 
-    switch(path) {
-        case '/first':
-            fs.readFile(__dirname + '/index.html', fsCallback)
-        break;
-        case '/second':
-            fs.readFile(__dirname + '/index2.html', fsCallback)
-        break;
-        default:
-            fs.readFile(__dirname + '/index.html', fsCallback)
-        break;
-    }
+    fs.readFile(__dirname + path, fsCallback)
 });
 
 server.listen(port, hostname, () => {
