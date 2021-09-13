89a7f438ca8f1af3597385dad73f8c0703170a14
norayrsoftware <norayr.software@gmail.com>
2021-09-13 11:40:23 +0400

    TCP connection wia NET module

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const http = require('http');
+const net = require('net');
 const fs = require('fs');
+const port = 8080;
+const host = '127.0.0.1';
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
 
-const server = http.createServer((req,res) => {
-    const path = req.url;
-    const fsCallback = (err,data) => {
-        if(err) {
-            res.end("We couldn't find your page :(")
-        };
-
-        res.end(data)
-    }
-
-    fs.readFile(__dirname + path, fsCallback)
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+let server = net.createServer(onClientConnection);
+server.listen(port,host, () => {
+    console.log('Wating for a connection')
 })
+
+function onClientConnection(socket) {
+    socket.on('data', (data) => {
+        let URI = data.toString().split(' ')[1];
+
+        const fsCallback = (err,data) => {
+            if(err){
+                socket.end("err")
+            }
+            socket.end(data)
+        }
+        fs.readFile(__dirname+URI, fsCallback)
+    })
+}
